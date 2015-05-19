9510f71e3aecbba88f067da43a2278ea2154c356
dpaschal <daniel.paschal@gmail.com>
2015-05-19 15:08:17 -0700

    filled out ajax more

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 $(document).ready(function(){
 	$('button').click(function(){
+		console.log('click initiated');
 		$.ajax({
+			dataType: 'json',
 			url: 'http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topMovies/json',
-			crossDomain: true,
-			success: function(){
-				console.log('loaded');
+			success: function(result){
+				console.log('loaded',result);
+
 			}
 		});
 	});
